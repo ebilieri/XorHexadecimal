169455d853f12a69aae1a19c4229f2dac3c8d966
Emerson Claudelino <48636325+ebilieri@users.noreply.github.com>
2020-11-09 10:57:09 -0300

    xor hexadecimal calc

---
+++ ConsoleApp4/HexString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace XorHexadecimal
+{
+    public class HexString
+    {
+        private byte[] _data;
+
+        public HexString(string data)
+        {
+            if ((data.Length & 1) != 0) throw new ArgumentException("Hex string must have an even number of digits.");
+
+            _data = Enumerable.Range(0, data.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(data.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        public HexString(byte[] data)
+        {
+            _data = data;
+        }
+
+
+        public override string ToString()
+        {
+            string hex = BitConverter.ToString(_data);
+            return hex.Replace("-", "");
+        }
+
+        static public HexString operator ^(HexString LHS, HexString RHS)
+        {
+            return new HexString
+                (
+                    LHS._data.Zip
+                        (
+                            RHS._data,
+                            (a, b) => (byte)(a ^ b)
+                        )
+                    .ToArray()
+                );
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XorHexadecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HexString bdk1 = new HexString("28c06eb41c4dc9c3ae114831efcac7446c8747777fca8b145ecd31ff8480ae88");
+            HexString bdk2 = new HexString("4d4abb9168114e349672b934d16ed201a919cb49e28b7f66a240e62c92ee007f");
+            HexString bdk3 = new HexString("fce514f84f37934bc8aa0f861e4f7392273d71b9d18e8209d21e4192a7842058");
+
+            const string expected = "996fc1dd3b6b14bcf0c9fe8320eb66d7e2a3fd874ccf767b2e939641b1ea8eaf";
+
+
+            List<HexString> list = new List<HexString>
+            {
+                //list.Add(bdk1);
+                bdk2,
+                bdk3
+            };
+
+            //Primeiro item hexadecimal
+            HexString hexString = new HexString("28c06eb41c4dc9c3ae114831efcac7446c8747777fca8b145ecd31ff8480ae88");
+            foreach (var item in list)
+            {
+                hexString ^= item;
+            }
+
+
+            HexString actual = bdk1 ^ bdk2 ^ bdk3;
+
+            Console.WriteLine("Expected: " + expected);
+            Console.WriteLine("Actual  : " + actual.ToString().ToLower());
+            Console.WriteLine("HexStr  : " + hexString.ToString().ToLower());
+
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
